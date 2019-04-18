852acae171ebef111d08f575aed73bfc2945a6f1
alexaoswald <alexaoswald1@gmail.com>
2019-04-18 17:00:56 -0400

    html

---
+++ logic.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCVlR9-u5OWVxxoPm4BTJEX8mxE2mmMU5Y",
+    authDomain: "train-scheduler-8109a.firebaseapp.com",
+    databaseURL: "https://train-scheduler-8109a.firebaseio.com",
+    projectId: "train-scheduler-8109a",
+    storageBucket: "train-scheduler-8109a.appspot.com",
+    messagingSenderId: "238314913128"
+  };
+  firebase.initializeApp(config);
